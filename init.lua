8c9c82452671ab2b1972d9c2babe1c4a36b98126
Fernando Paredes Garcia <fernando@develcuy.com>
2014-03-14 20:25:44 -0500

    Render social links on hook content_render().

--- init.lua
+++ init.lua
@@ -9,6 +9,20 @@ module 'ophal.modules.social'
 local js_added
 local css_added
 
+--[[
+  Implements hook_content_load().
+]]
+function content_render(content)
+  content.social_links = tconcat{'<div id="social">',
+    theme{'item_list', list = {
+      get_social_button('googleplus-one', url('content/' .. content.id), '', {['data-size'] = 'tall'}),
+      get_social_button('twitter-share', '#', content.title, {['data-related'] = 'MisMoldesGratis', ['data-count'] = 'vertical'}),
+      get_social_button('facebook-like', '#', '', {['data-layout'] = 'box_count', ['data-width'] = 60, ['data-show-faces'] = 'false', ['data-send'] = 'false'}),
+    }},
+    '</div>',
+  }
+end
+
 function get_social_button(widget, url, text, attributes)
   if attributes == nil then
     attributes = {}
